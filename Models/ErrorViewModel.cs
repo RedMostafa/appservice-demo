b6c34785600523c0a19594d2c1a03b6ff5545e69
Reda Mostafa <Reda@MngEnvMCAP557965.onmicrosoft.com>
2024-11-10 08:07:11 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Webapp_VS_Code_2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
